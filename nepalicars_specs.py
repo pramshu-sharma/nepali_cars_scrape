3b21ae7fd02a8b995cf17231bff84c7d23f99abe
pramshu-sharma <pramshu.sharma35@gmail.com>
2023-07-27 15:18:42 +0545

    scrape attribute list added

--- nepalicars_specs.py
+++ nepalicars_specs.py
@@ -1,6 +1,17 @@
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from bs4 import BeautifulSoup
+'''
+
+
+ad create date
+views
+all specs
+seller name
+seller contact
+'''
+
+
 
 url = 'https://www.nepalicars.com/en/vehicle_listings/ad-maruti-wagon-r-bagmati-bhaktapur-4326'
 chrome_options = Options()
@@ -11,6 +22,10 @@ page_source = driver.page_source
 driver.quit()
 
 soup = BeautifulSoup(page_source, 'html.parser')
-prop = soup.find('div', class_='vehicle-properties').find_all('span')
-print(prop)
+title = soup.find('div', class_='ad-title').find('h2').text
+location = soup.find('div', class_='ad-title').find('span').text
+npr_price = soup.find('div', class_='ad-price').find('span', class_='price').text
+usd_price = soup.find('div', class_='accordion-sub-card free').find('span', class_='price').text
+create_date = soup.find('div', class_='ad-created-border').find('strong').text
+
 
